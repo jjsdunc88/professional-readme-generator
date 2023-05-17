2e623788cbed22a906c0e2a5b9a4aa79018908e8
JDunc <jjsduncan@gmail.com>
2023-05-17 12:42:15 -0400

    update commentary, clean up

--- index.js
+++ index.js
@@ -3,13 +3,8 @@ const generateMarkdown = require("./utils/generateMarkdown")
 const inquirer = require("inquirer")
 const fs = require("fs")
 
-// //Array of questions for user input
-// const questions = [];
 
-// //Function to write README file
-// function writeToFile(fileName, data) { }
-
-// TODO: Create a function to initialize app
+//Function to initialize app
 function init() {
     inquirer.prompt([
         {
